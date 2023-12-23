9e377493c57fb3db8245f9193ea73fb30f4d5e85
ali.parsaee1 <ali.parsaee1@cs.ucalgary.ca>
2023-12-22 22:30:39 -0600

    added gpt talking capabilities, voice to text, text to voice, and combined it into one webpage in index.html

--- stash/server.js
+++ stash/server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(express.static('public')); // Serve static files from 'public' directory
 
 const openai = new OpenAIApi({
-  apiKey: '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',
+  apiKey: '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',
 });
 
 app.post('/ask', async (req, res) => {
@@ -16,7 +16,10 @@ app.post('/ask', async (req, res) => {
             model: "gpt-3.5-turbo",
             messages: [{ role: 'user', content: req.body.query }],
         });
-        res.json({ answer: response.data.choices[0].text.trim() });
+        console.log("OpenAI API response:", response);
+        const answer = response.choices[0].message.content.trim();
+        res.json({ answer: answer });
+
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'An error occurred', details: error.message });
